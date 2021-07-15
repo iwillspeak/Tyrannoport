0c93b3e3f04d0e082a548a4c873e00344015d83f
Will Speak <lithiumflame@gmail.com>
2021-07-15 18:39:42 +0100

    Scaffold Exe

---
+++ src/Tryanoport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tryanoport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
