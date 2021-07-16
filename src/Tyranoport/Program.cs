e6798156a66655f83c5530240e910cf6b493703c
Will Speak <lithiumflame@gmail.com>
2021-07-16 18:12:55 +0100

    Initial Liquid Rendering

--- src/Tyranoport/Program.cs
+++ src/Tyranoport/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using Tyranoport.Trx;
+using Tyranoport.Trx.Models;
+using DotLiquid;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
 
-namespace Tryanoport
+namespace Tyranoport
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             foreach (var path in args)
             {
                 var loaded = TrxReader.LoadPath(path);
+                await RenderAsync(loaded, Path.ChangeExtension(path, "html"));
             }
+        }
+
+        private static async Task RenderAsync(TestRun report, string outputPath)
+        {
+            using var templateStream = new StreamReader(typeof(Program).Assembly.GetManifestResourceStream("Tyranoport.templates.index.liquid")!);
+            var template = Template.Parse(await templateStream.ReadToEndAsync());
+            using var output = File.OpenWrite(outputPath);
+
+            template.Render(output, new RenderParameters(CultureInfo.InvariantCulture)
+            {
+                LocalVariables = Hash.FromAnonymousObject(report),
+            });
         }
     }
 }
