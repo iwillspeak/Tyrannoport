936b9e9fb2c4b2dbfe20974e3db1bfdbb4e648f1
Will Speak <lithiumflame@gmail.com>
2021-07-17 11:50:23 +0100

    Switch to OctoVersion
    Fed up of GitVersion. It doesn't play well with VS Code.

--- build.sh
+++ build.sh
@@ -1,26 +1,22 @@
 #! /usr/bin/env bash
 
-if [ -n "$TF_BUILD" ]
+if [ -z "$BUILD_SOURCEBRANCH" ]
 then
-    # normalise our branches
-    echo "Initialising CI branch state"
-    git branch --track main origin/main
-    case "$BUILD_SOURCEBRANCH" in
-        refs/heads/*)
-            branch_name=${BUILD_SOURCEBRANCH:11}
-            git switch --force-create "$branch_name" HEAD
-            ;;
-        *)
-            git switch --force-create "ci" HEAD
-            ;;
-    esac
+    echo "Initialising local branch state"
+    BUILD_SOURCEBRANCH=`git branch --show-current`
 fi
 
 set -euox pipefail
 
 dotnet --version --verbose
 
-dotnet build --configuration Release
-dotnet test --no-build --configuration Release  --logger 'trx' --logger 'console;verbosity=normal'
-dotnet pack -o PublishOutput --configuration Release
+## Generate Version Information
+dotnet tool restore
+export $(dotnet tool run octoversion --CurrentBranch=${BUILD_SOURCEBRANCH} --OutputFormats:0=Environment | xargs)
+versionFlags="/P:Version=${OCTOVERSION_NuGetVersion} /P:InformationalVersion=${OCTOVERSION_InformationalVersion}"
+
+## Run the Build
+dotnet build --configuration Release $versionFlags
+dotnet test --no-build --configuration Release $versionFlags --logger 'trx' --logger 'console;verbosity=normal'
+dotnet pack -o PublishOutput --configuration Release $versionFlags
 
