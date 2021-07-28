19eed653c8e8b54c8645eac8f0a925f3199497fc
Will Speak <lithiumflame@gmail.com>
2021-07-28 15:49:12 +0100

    Implement Tool Runner

---
+++ src/Cake.Tyrannoport/TyrannoportSettings.cs
@@ -0,0 +1,9 @@
+using Cake.Core.Tooling;
+
+namespace Cake.Tyrannoport
+{
+    /// <summary>Cake Settings for the Tyrannoport tool</summary>
+    public class TyrannoportSettings : ToolSettings
+    {
+    }
+}
